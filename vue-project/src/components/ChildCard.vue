d4a4a345bcce9301c04bfc474ce1073cc3f831b8
falconve <falconve@gmail.com>
2025-08-04 08:48:44 +0700

    emit

--- vue-project/src/components/ChildCard.vue
+++ vue-project/src/components/ChildCard.vue
@@ -49,58 +49,6 @@ function remove(id) {
 </script>
 
 <template>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="card mx-auto" style="max-width: 400px">
-        <div class="card-body">
-          <h3 class="card-title text-center mb-4">Quản lý công việc</h3>
-
-          <div class="mb-3">
-            <label class="form-label">Tên công việc:</label>
-            <input
-              type="text"
-              class="form-control"
-              placeholder="Nhập tên công việc"
-            />
-          </div>
-
-          <div class="d-grid mb-3">
-            <button class="btn btn-primary">Lưu</button>
-          </div>
-
-          <ul class="list-group">
-            <li
-              class="list-group-item d-flex justify-content-between align-items-center"
-            >
-              Ăn sáng
-              <div>
-                <button class="btn btn-secondary btn-sm me-2">Sửa</button>
-                <button class="btn btn-danger btn-sm">Xóa</button>
-              </div>
-            </li>
-            <li
-              class="list-group-item d-flex justify-content-between align-items-center"
-            >
-              Đi học
-              <div>
-                <button class="btn btn-secondary btn-sm me-2">Sửa</button>
-                <button class="btn btn-danger btn-sm">Xóa</button>
-              </div>
-            </li>
-            <li
-              class="list-group-item d-flex justify-content-between align-items-center"
-            >
-              Chơi bóng rổ
-              <div>
-                <button class="btn btn-secondary btn-sm me-2">Sửa</button>
-                <button class="btn btn-danger btn-sm">Xóa</button>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
   <Emits
     @confirmButton="display"
     :schoolName="name"
